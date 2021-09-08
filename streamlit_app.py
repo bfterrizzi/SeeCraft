d81a32a568b8749b8b0d212385cf0d2e14d25ed7
bfterrizzi <bfterrizzi@gmail.com>
2021-09-08 14:01:27 -0500

    Changes to app title, header, and body text.

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,20 +6,20 @@ from img_classification import seecraft_classification
 from PIL import Image
 
 
-st.title("Image Classification with SeeCraft")
-st.header("Detecting Seacraft Example")
-st.text("Upload an Image to be classified as 'Ship Absent' or 'Ship Present'")
+st.title("SeeCraft")
+st.header("AI powered ship detection in satellite imagery")
+st.text("The ocean is so large enough that monitoring it from the ground is ineffective. SeeCraft provides a solution to this problem by connecting the people responsible for safeguarding our oceans to a powerful algorithm designed to detect the presence of ships in remote satellite imagery.")
 
 
-uploaded_file = st.file_uploader("Choose a satellite image ...", type="jpg")
+uploaded_file = st.file_uploader("Choose an image to inspect...", type="jpg")
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
     st.image(image, caption='Uploaded Image.', use_column_width=True)
-    st.write("")
+    st.write("Studying Image...")
     st.write("Classifying...")
     label = seecraft_classification(image, '')
     if label == 0:
-        st.write("Ship Absent: The image does not contain a ship.")
+        st.write("Ship Absent: This image does not contain a ship.")
     else:
-        st.write("Ship Present: The image contains a ship!")
+        st.write("Ship Present: This image contains a ship!")
